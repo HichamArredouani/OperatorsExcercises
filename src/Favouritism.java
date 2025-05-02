f17b13f6808b45d0d3648ab1d2d80db863b6f903
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-02 16:39:34 +0200

    Favoriete naam

--- src/Favouritism.java
+++ src/Favouritism.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Favouritism {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your first name: ");
+        String Username = input.nextLine();
+
+        String FavouriteName = "Hicham";
+
+        if (Username.equals(FavouriteName)) {
+        System.out.println("Aywaaaa baz hlik");
+} else {
+            System.out.println("Maar alle zeg wie is da makkakske nu" );
+        }
+
+    }
 }

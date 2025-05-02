5ae8b51895d98941ea20ad813441085acc6b302b
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-02 16:23:41 +0200

    nieuwe update van Postzegel oefening

--- src/PostageStamps.java
+++ src/PostageStamps.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class PostageStamps {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your letter's mass: ");
+        double massa = input.nextDouble();
+
+        int zegels = 1;
+        if (massa > 40) {
+            zegels = 2;
+        }
+    System.out.println("Je hebt " + zegels +" zegels nodig. ");
+
+}
 }
